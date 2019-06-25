13213f2964e848b628dfe1c8938cc788e9f76b3d
Pmiintellect <pm@iintelect.co.uk>
2019-06-25 15:35:02 +0500

    Research page complete

--- src/pages/providers.js
+++ src/pages/providers.js
@@ -4,19 +4,75 @@ import SEO from "../components/seo";
 import Main from "../components/providerMain";
 import DetailCard from "../components/detailComponent";
 import Partners from "../components/aboutPartners"
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { graphql } from 'gatsby';
 
-const Families = () => (
-    <Layout>
-        <SEO title="Providers"/>
-        <Main title="Learn how WEconnect reduces relapse rates, increases treatment efficiency and provides support and accountability to patients." image="/images/WEC_Providers.webp"/>
-        <div className="provider-detail-section">
-            <DetailCard img={"/images/laptop-2.webp"} swap={true}  content={"<h1>Utilize our powerful, data-driven dashboard.</h1><p>Designed and built specificially for treatment centers to easily manage recovery groups. This powerful dashboard includes a personalized risk score for each patient ‐ plus critical data on patients, groups or multiple treatment centers all in one place.</p>"}/>
-            <DetailCard img={"/images/map-card.webp"} swap={false} content={"<h1>Analyze live outcomes data and manage JCAHO.</h1><p>Analyze dozens of data points to improve and assess programs. Maintain joint commission compliance certification with verified outcomes data.</p>"}/>
-            <DetailCard img={"/images/graph.webp"} swap={true} content={"<h1>Spend time on treatment, not guesswork</h1><p>Instantly be alerted to changes in patient routines, achievements and more. Track group and individual progress, and review gaps in scheduling to recommend care.</p>"}/>
-            <Partners/>
-        </div>
+const Families = ({ data }) => {
+    const pageData = data.contentfulPage;
+    const contentMarkup = documentToHtmlString(JSON.parse(pageData.content.content));
+    return (
+        <Layout>
+            <SEO title="Providers" />
+            <Main title={contentMarkup.replace(/(<([^>]+)>)/ig, "")} image={"https:" + pageData.headerBackground.file.url} />
+            <div className="provider-detail-section">
+                {
+                    pageData.pageBlock.map((single, i) => (
+                        <DetailCard key={i} img={"https:" + single.image.file.url} swap={i % 2 === 0} content={documentToHtmlString(JSON.parse(single.content.content))} />
+                    ))
+                }
+                <Partners />
+            </div>
 
-    </Layout>
-)
+        </Layout>
+    )
+}
 
 export default Families;
+
+export const PageQuery = graphql`
+    query ProviderspageQuery {
+        contentfulPage ( slug: { eq: "providers" } ) {
+            title
+            slug
+            content {
+                content
+            }
+            headerBackground {
+                file {
+                    url
+                }
+            }
+            headerBanner {
+                file {
+                    url
+                }
+            }
+            featureImage {
+                file {
+                    url
+                }
+            }
+            featureText {
+                featureText
+            }
+            pageBlock {
+                title
+                content { 
+                    content
+                }
+                image {
+                    file {
+                        url
+                    }
+                }
+                linkText
+                linkTo
+                icon {
+                    file {
+                        url
+                    }
+                }
+            }
+        }
+    }
+`
--- src/pages/research.js
+++ src/pages/research.js
@@ -3,18 +3,75 @@ import Layout from "../components/layout"
 import SEO from "../components/seo";
 import Main from "../components/providerMain";
 import DetailCard from "../components/detailComponent";
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { graphql } from 'gatsby';
 
-const Families = () => (
-    <Layout>
-        <SEO title="Families"/>
-        <Main title="Scientifically Proven Addiction Relapse Prevention" image="/images/WEC_Research.webp"/>
-        <div className="provider-detail-section">
-            <DetailCard img={"/images/science-photo-one.webp"} swap={true}  content={"<h1>Identifying Relapse Before It Happens</h1><p>WEconnect’s proprietary risk score algorithm identifies relapse before it happens, so support networks can step in. Our approach is positive reinforcement with no shaming. WEconnect leverages only positive rewards and reinforcement to ensure the best outcomes for patients. We are backed by proven scientific methods by leading scientists, PhD researchers, clinicians and treatment experts based on the latest evidence-based methods.</p>"}/>
-            <DetailCard img={"/images/science-photo-two.webp"} swap={false} content={"<h1>Case Management</h1><p>Counselors can view group and individual progress including predictive indicators for patients who are at high risk of relapse. Counselors receive alerts when patients are missing treatment plan activities, and can view gaps in scheduling to address issues ahead of time. Case managers and therapists can have meaningful conersations with clients that have hit milestones by completing activities.</p>"}/>
-            <DetailCard img={"/images/science-photo-three.webp"} swap={true} content={"<h1>Track Patient Outcomes</h1><p>Dashboards provide visual breakdowns to easily track HIPAA compliant data. Providers can access pre-made patient panel data for outcomes studies and insights into treatment programs, using 45 different data points.</p>"}/>
-        </div>
+const Families = ({ data }) => {
+    console.log(data);
+    const pageData = data.contentfulPage;
+    const contentMarkup = documentToHtmlString(JSON.parse(pageData.content.content));
+    return (
+        <Layout>
+            <SEO title="Families" />
+            <Main title={contentMarkup.replace(/(<([^>]+)>)/ig, "")} image={"https:" + pageData.headerBackground.file.url} />
+            <div className="provider-detail-section">
+                {
+                    pageData.pageBlock.map((single, i) => (
+                        <DetailCard key={i} img={"https:" + single.image.file.url} swap={i % 2 === 0} content={documentToHtmlString(JSON.parse(single.content.content))} />
+                    ))
+                }
+            </div>
 
-    </Layout>
-)
+        </Layout>
+    );
+}
 
 export default Families;
+
+export const PageQuery = graphql`
+    query ResearchpageQuery {
+        contentfulPage ( slug: { eq: "research" } ) {
+            title
+            slug
+            content {
+                content
+            }
+            headerBackground {
+                file {
+                    url
+                }
+            }
+            headerBanner {
+                file {
+                    url
+                }
+            }
+            featureImage {
+                file {
+                    url
+                }
+            }
+            featureText {
+                featureText
+            }
+            pageBlock {
+                title
+                content { 
+                    content
+                }
+                image {
+                    file {
+                        url
+                    }
+                }
+                linkText
+                linkTo
+                icon {
+                    file {
+                        url
+                    }
+                }
+            }
+        }
+    }
+`
